9a011924ff2750461756e5832b32ff207e8aabf8
fanybook <fanybook@gmail.com>
2017-06-16 11:18:06 +0800

    mod php array_reset_idx

--- php/array_reset_idx.php
+++ php/array_reset_idx.php
@@ -15,15 +15,16 @@ var_dump(array_reset_idx($arr, 'name', 2));
 /**
  * 返回以原数组某个值为下标的新数据
  *
- * @param array $array
- * @param string $key
- * @param int $type 1一维数组2二维数组
+ * @param  array    $arr
+ * @param  string   $idx
+ * @param  int      $type       1:一维数组 2:二维数组
  * @return array
  */
 function array_reset_idx($arr, $idx, $type = 1)
 {
     if (is_array($arr)) {
         $tmp_arr = [];
+
         foreach ($arr as $val) {
             if ($type === 1) {
                 $tmp_arr[$val[$idx]] = $val;
@@ -31,6 +32,7 @@ function array_reset_idx($arr, $idx, $type = 1)
                 $tmp_arr[$val[$idx]][] = $val;
             }
         }
+
         ksort($tmp_arr);
         return $tmp_arr;
     }
